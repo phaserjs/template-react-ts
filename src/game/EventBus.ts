b13d149cb89ff9d692a78f1ddcc9064e4d380721
Francisco Pereira Alvarado <gammafp@gmail.com>
2024-03-22 23:20:39 +0100

    update phaser imports

--- src/game/EventBus.ts
+++ src/game/EventBus.ts
@@ -1,5 +1,5 @@
-import Phaser from 'phaser';
+import { Events } from 'phaser';
 
 // Used to emit events between React components and Phaser scenes
 // https://newdocs.phaser.io/docs/3.70.0/Phaser.Events.EventEmitter
-export const EventBus = new Phaser.Events.EventEmitter();
+export const EventBus = new Events.EventEmitter();
--- src/game/main.ts
+++ src/game/main.ts
@@ -2,13 +2,13 @@ import { Boot } from './scenes/Boot';
 import { GameOver } from './scenes/GameOver';
 import { Game as MainGame } from './scenes/Game';
 import { MainMenu } from './scenes/MainMenu';
-import Phaser from 'phaser';
+import { AUTO, Game } from 'phaser';
 import { Preloader } from './scenes/Preloader';
 
 //  Find out more information about the Game Config at:
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
+    type: AUTO,
     width: 1024,
     height: 768,
     parent: 'game-container',
@@ -24,7 +24,7 @@ const config: Phaser.Types.Core.GameConfig = {
 
 const StartGame = (parent: string) => {
 
-    return new Phaser.Game({ ...config, parent });
+    return new Game({ ...config, parent });
 
 }
 
